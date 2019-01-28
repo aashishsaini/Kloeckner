3f62aa9c57200c214417b3c847ae74c8a201d205
Aashish Saini <aashusaini2684@gmail.com>
2019-01-28 15:50:11 +0530

    TDD: initial, all fail

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should not save product without part_number" do
+    product = Product.new
+    assert_not product.save
+  end
+
+  test "should save product with part_number" do
+    product = Product.new(part_number: 'ID101034')
+    assert product.save
+  end
+
+  test "parse the csv and return the collection of rows" do
+    all_rows = Product.extract_rows(Rails.root.join('data.csv'))
+    assert_kind_of(Array, all_rows)
+  end
+
+  test "should return true for valid CSV" do
+    response = Product.process_csv(Rails.root.join('data.csv'), ',', 'false')
+    assert_not response
+  end
 end
